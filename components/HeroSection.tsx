1b780c39d0ff08105d89eab022cd396684740711
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-12-25 14:56:01 +0700

    fix hero section padding

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -34,7 +34,7 @@ function HeroSection() {
   });
 
   return (
-    <div className="flex flex-wrap gap-5 p-4 md:p-0 justify-center w-screen">
+    <div className="">
       <HeroHighlight>
         <motion.h1
           initial={{
@@ -60,132 +60,136 @@ function HeroSection() {
         </motion.h1>
       </HeroHighlight>
 
-      <Card
-        id="about"
-        className="bg-secondary/5 w-screen lg:max-w-[50%] md:p-7 md:rounded-3xl"
-      >
-        <CardHeader>
-          <motion.div
-            variants={upAnimation(0)}
-            initial="initial"
-            whileInView="animate"
-            viewport={{ amount: 0.5 }}
-            className="flex justify-between flex-wrap"
-          >
-            <div className="flex gap-3">
-              <Avatar className="h-16 w-16">
-                <AvatarImage
-                  src="avatar\avatar-blink.png"
-                  className="-translate-y-2"
-                />
-              </Avatar>
-              <div className="flex flex-col">
-                <span className="font-bold">Hey i'm Ikhsan.</span>
-                <span className="text-foreground/50">
-                  Computer Engineering student at University of Indonesia
-                </span>
+      <div className="p-4 md:p-0 flex flex-wrap gap-5 justify-center w-screen">
+        <Card
+          id="about"
+          className="bg-secondary/5 w-screen lg:max-w-[50%] md:p-7 md:rounded-3xl"
+        >
+          <CardHeader>
+            <motion.div
+              variants={upAnimation(0)}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ amount: 0.5 }}
+              className="flex justify-between flex-wrap"
+            >
+              <div className="flex gap-3">
+                <Avatar className="h-16 w-16">
+                  <AvatarImage
+                    src="avatar\avatar-blink.png"
+                    className="-translate-y-2"
+                  />
+                </Avatar>
+                <div className="flex flex-col">
+                  <span className="font-bold">Hey i'm Ikhsan.</span>
+                  <span className="text-foreground/50">
+                    Computer Engineering student at University of Indonesia
+                  </span>
+                </div>
               </div>
-            </div>
-            <div className="flex gap-3 ">
-              <a href="https://github.com/esuuun">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="transition ease-in-out duration-700  hover:bg-transparent hover:ring-4 ring-muted "
-                >
-                  <Github className="h-4 w-4" />
-                </Button>
-              </a>
-              <a href="https://www.linkedin.com/in/muhammad-ikhsan-kurniawan-0b763028a/">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="transition ease-in-out duration-700  hover:bg-transparent hover:ring-4 ring-muted "
-                >
-                  <Linkedin className="h-4 w-4" />
-                </Button>
-              </a>
+              <div className="flex gap-3 ">
+                <a href="https://github.com/esuuun">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="transition ease-in-out duration-700  hover:bg-transparent hover:ring-4 ring-muted "
+                  >
+                    <Github className="h-4 w-4" />
+                  </Button>
+                </a>
+                <a href="https://www.linkedin.com/in/muhammad-ikhsan-kurniawan-0b763028a/">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="transition ease-in-out duration-700  hover:bg-transparent hover:ring-4 ring-muted "
+                  >
+                    <Linkedin className="h-4 w-4" />
+                  </Button>
+                </a>
 
-              <a href="https://www.instagram.com/ikhsankwn/">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="transition ease-in-out duration-700 hover:bg-transparent hover:ring-4 ring-muted"
-                >
-                  <Instagram className="h-4 w-4" />
-                </Button>
-              </a>
-              <a href="https://www.linkedin.com/in/muhammad-ikhsan-kurniawan-0b763028a/">
-                <Button
-                  variant="outline"
-                  className="font-semibold text-base transition ease-in-out duration-700 hover:bg-transparent hover:ring-4 ring-muted"
-                >
-                  Get In Touch
-                </Button>
-              </a>
-            </div>
-          </motion.div>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-3">
-          <motion.p
-            variants={upAnimation(0.2)}
-            initial="initial"
-            whileInView="animate"
-            viewport={{ amount: 0 }}
-            className="text-4xl font-semibold"
-          >
-            Passionate about crafting{" "}
-            <span className="italic font-serif font-normal">exceptional </span>{" "}
-            digital experiences.
-          </motion.p>
-        </CardContent>
-        <CardFooter>
-          <motion.p
-            variants={upAnimation(0.3)}
-            initial="initial"
-            whileInView="animate"
-            viewport={{ amount: 1 }}
-            className="text-xl font-medium text-foreground/60"
-          >
-            Hey!, I’m Ikhsan! Welcome to my world. I specialize in designing and
-            developing beautiful, user-friendly websites and digital
-            experiences. With a focus on clean design and seamless
-            functionality, I aim to create solutions that leave a lasting
-            impression.
-          </motion.p>
-        </CardFooter>
-      </Card>
+                <a href="https://www.instagram.com/ikhsankwn/">
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="transition ease-in-out duration-700 hover:bg-transparent hover:ring-4 ring-muted"
+                  >
+                    <Instagram className="h-4 w-4" />
+                  </Button>
+                </a>
+                <a href="https://www.linkedin.com/in/muhammad-ikhsan-kurniawan-0b763028a/">
+                  <Button
+                    variant="outline"
+                    className="font-semibold text-base transition ease-in-out duration-700 hover:bg-transparent hover:ring-4 ring-muted"
+                  >
+                    Get In Touch
+                  </Button>
+                </a>
+              </div>
+            </motion.div>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-3">
+            <motion.p
+              variants={upAnimation(0.2)}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ amount: 0 }}
+              className="text-4xl font-semibold"
+            >
+              Passionate about crafting{" "}
+              <span className="italic font-serif font-normal">
+                exceptional{" "}
+              </span>{" "}
+              digital experiences.
+            </motion.p>
+          </CardContent>
+          <CardFooter>
+            <motion.p
+              variants={upAnimation(0.3)}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ amount: 1 }}
+              className="text-xl font-medium text-foreground/60"
+            >
+              Hey!, I’m Ikhsan! Welcome to my world. I specialize in designing
+              and developing beautiful, user-friendly websites and digital
+              experiences. With a focus on clean design and seamless
+              functionality, I aim to create solutions that leave a lasting
+              impression.
+            </motion.p>
+          </CardFooter>
+        </Card>
 
-      {/* img */}
-      <Card className="relative bg-gradient-to-br from-muted to-card w-screen lg:max-w-sm rounded-3xl overflow-hidden">
-        <div className="absolute inset-0 z-0">
-          <Canvas>
-            <Stars
-              radius={100}
-              depth={50}
-              count={800}
-              factor={4}
-              saturation={0}
-              fade
-              speed={0.5}
+        {/* img */}
+        <Card className="relative bg-gradient-to-br from-muted to-card w-screen lg:max-w-sm rounded-3xl overflow-hidden">
+          <div className="absolute inset-0 z-0">
+            <Canvas>
+              <Stars
+                radius={100}
+                depth={50}
+                count={800}
+                factor={4}
+                saturation={0}
+                fade
+                speed={0.5}
+              />
+            </Canvas>
+          </div>
+
+          <div className="relative z-10 flex justify-center items-end h-full">
+            <motion.img
+              variants={upAnimation(0.5)}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ amount: 0.5 }}
+              src="avatar/avatar-hi.png"
+              alt=""
+              className="h-80 object-cover"
             />
-          </Canvas>
-        </div>
-
-        <div className="relative z-10 flex justify-center items-end h-full">
-          <motion.img
-            variants={upAnimation(0.5)}
-            initial="initial"
-            whileInView="animate"
-            viewport={{ amount: 0.5 }}
-            src="avatar/avatar-hi.png"
-            alt=""
-            className="h-80 object-cover"
-          />
-          <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-card via-card/70 to-transparent z-20" />
-        </div>
-        <Meteors number={5} />
-      </Card>
+            <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-card via-card/70 to-transparent z-20" />
+          </div>
+          <Meteors number={5} />
+        </Card>
+      </div>
     </div>
   );
 }
